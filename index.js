beba9d6e74eb8f0d39a9646380b620862607438e
CJ-webdev <coinjimmy@gmail.com>
2022-10-02 19:05:14 +0200

    site john debut

--- index.js
+++ index.js
@@ -1,24 +1,12 @@
-const form = document.querySelector("form");
-
-function storeList() {
-  window.localStorage.todoList = list.innerHTML;
-}
-
-function getTodos() {
-  list.innerHTML = window.localStorage.todoList;
-}
-
-getTodos();
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  list.innerHTML += `<li><span>*</span> ${item.value}</li>`;
-  item.value = "";
-  storeList();
-});
-
-list.addEventListener("click", (e) => {
-  e.target.remove();
-  storeList();
+var swiper = new Swiper(".mySwiper", {
+  cssMode: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  mousewheel: true,
+  keyboard: true,
 });
