a33ba95f5af836c28e21b40531b5cf1713b435a3
CJ-webdev <coinjimmy@gmail.com>
2022-10-17 12:28:22 +0200

    essai emailJs

--- index.js
+++ index.js
@@ -12,3 +12,22 @@ var swiper = new Swiper(".mySwiper", {
   mousewheel: true,
   keyboard: true,
 });
+
+window.onload = function () {
+  document
+    .getElementById("contact-form")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+      // generate a five digit number for the contact_number variable
+      this.contact_number.value = (Math.random() * 100000) | 0;
+      // these IDs from the previous steps
+      emailjs.sendForm("service_71elrfl", "contact_form", this).then(
+        function () {
+          console.log("SUCCESS!");
+        },
+        function (error) {
+          console.log("FAILED...", error);
+        }
+      );
+    });
+};
