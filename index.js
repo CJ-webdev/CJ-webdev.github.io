8316202ed31fd4ed0271dd75f521e75dddd333a7
CJ-webdev <coinjimmy@gmail.com>
2022-11-23 11:08:28 +0100

    prmier changement du 23 novembre

--- index.js
+++ index.js
@@ -12,3 +12,30 @@ var swiper = new Swiper(".mySwiper", {
   mousewheel: true,
   keyboard: true,
 });
+
+const openBtn = document.getElementById("open-btn");
+const closeBtn = document.getElementById("close-btn");
+const section0 = document.querySelector(".section0");
+const aRemove1 = document.querySelector(".a-remove-1");
+const aRemove2 = document.querySelector(".a-remove-2");
+const aRemove3 = document.querySelector(".a-remove-3");
+
+openBtn.addEventListener("click", (e) => {
+  section0.setAttribute("id", "open-section-0");
+});
+
+closeBtn.addEventListener("click", (e) => {
+  section0.removeAttribute("id", "open-section-0");
+});
+
+aRemove1.addEventListener("click", (e) => {
+  section0.removeAttribute("id", "open-section-0");
+});
+
+aRemove2.addEventListener("click", (e) => {
+  section0.removeAttribute("id", "open-section-0");
+});
+
+aRemove3.addEventListener("click", (e) => {
+  section0.removeAttribute("id", "open-section-0");
+});
